f12b45d8b1488d2b9813dba369dfeef6e2ccc93d
JPauloMoura <joaopaulo.034a@gmail.com>
2021-04-08 16:22:41 -0300

    strings e regex

---
+++ Csharp_String_Regx_Obj/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            try
+            {
+                ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
+
+                string moedaOrigem = extrator.GetValor("mOedaOrigem");
+                Console.WriteLine("Moeda origem: " + moedaOrigem);
+
+                string moedaDestino = extrator.GetValor("MoedaDestino");
+                Console.WriteLine("Moeda destino: " + moedaDestino);
+                Console.WriteLine("-------------------------------------");
+
+                ContaCorrente conta = new ContaCorrente(123, 98989);
+                ContaCorrente conta2 = new ContaCorrente(113, 98989);
+                Console.WriteLine(conta.GetType());
+                Console.WriteLine(conta.Equals(conta2));
+                Console.WriteLine(conta.GetHashCode()); //46104728
+                Console.WriteLine(conta.ToString()); 
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+         
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
